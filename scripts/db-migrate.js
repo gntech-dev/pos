827fdbbc239b58326fec0efbbb8cb1a0ffe47e08
gntech-dev <gerlin.nolasco@unicaribe.edu.do>
2025-12-29 19:35:50 +0000

    Remove debug logging now that issue is identified and fixed

--- scripts/db-migrate.js
+++ scripts/db-migrate.js
@@ -23,9 +23,6 @@ while (projectRoot !== path.dirname(projectRoot)) {
 
 // Change to project root
 process.chdir(projectRoot);
-console.log('Current working directory:', process.cwd());
-console.log('Project root:', projectRoot);
-console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
 // Run prisma migrate
 try {
